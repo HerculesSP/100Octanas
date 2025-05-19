55e8ba2d764103e08a3cf8b7ad29e425531d8470
Hércules <herculespereira99@gmail.com>
2025-05-18 22:00:24 -0300

    Modelo lógico 1.0.8

--- src/database/cemOctanas v1.0.4.sql
+++ src/database/cemOctanas v1.0.4.sql
@@ -4,59 +4,57 @@
 CREATE DATABASE IF NOT EXISTS cemOctanas ;
 
 -- -----------------------------------------------------
--- Tabela Cargo
+-- Tabela cemOctanas.Cargo
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Cargo (
-  idCargo INT NOT NULL,
-  descricao VARCHAR(45) NOT NULL,
+  idCargo INT NOT NULL AUTO_INCREMENT,
+  descricao VARCHAR(70) NOT NULL,
   PRIMARY KEY (idCargo)
 );
 
 
 -- -----------------------------------------------------
--- Tabela Usuario
+-- Tabela cemOctanas.Usuario
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Usuario (
-  idUsuario INT NOT NULL,
-  nome VARCHAR(45) NOT NULL,
-  sobrenome VARCHAR(45) NOT NULL,
-  senha VARCHAR(45) NOT NULL,
-  verificado TINYINT(1) NOT NULL DEFAULT 0,
-  foto VARCHAR(45) NULL,
+  idUsuario INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(75) NOT NULL,
+  sobrenome VARCHAR(150) NOT NULL,
+  senha VARCHAR(20) NOT NULL,
   dtNasc DATE NOT NULL,
-  dtInsc DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ativo TINYINT(1) NOT NULL DEFAULT 1,
-  fkCargo INT DEFAULT NULL,
+  fkCargo INT NULL,
+  PRIMARY KEY (idUsuario),
+  INDEX idx_nome (nome),
   CONSTRAINT fk_Usuario_Cargo
     FOREIGN KEY (fkCargo)
-    REFERENCES cemOctanas.Cargo(idCargo),
-  PRIMARY KEY (idUsuario)
+    REFERENCES cemOctanas.Cargo (idCargo)
 );
 
 
 -- -----------------------------------------------------
--- Tabela Imagem
+-- Tabela cemOctanas.Capa
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Capa (
-  idCapa INT NOT NULL,
-  link VARCHAR(45) NOT NULL,
-  legenda VARCHAR(45) NULL,
+  idCapa INT NOT NULL AUTO_INCREMENT,
+  link VARCHAR(100) NOT NULL,
+  legenda VARCHAR(75) NULL,
   PRIMARY KEY (idCapa)
 );
 
 
 -- -----------------------------------------------------
--- Tabela Materia
+-- Tabela cemOctanas.Materia
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Materia (
-  idMateria INT NOT NULL,
-  titulo VARCHAR(45) NOT NULL,
-  resumo VARCHAR(45) NOT NULL,
-  link VARCHAR(45) NOT NULL,
-  visivel TINYINT(1) NOT NULL DEFAULT 1,
+  idMateria INT NOT NULL AUTO_INCREMENT,
+  titulo VARCHAR(100) NOT NULL,
+  resumo VARCHAR(300) NOT NULL,
+  link VARCHAR(120) NOT NULL,
+  visivel TINYINT NOT NULL DEFAULT 1,
   fkAutor INT NOT NULL,
   fkCapa INT NOT NULL,
   PRIMARY KEY (idMateria),
+  INDEX idx_titulo (titulo),
   CONSTRAINT fk_Materia_Autor
     FOREIGN KEY (fkAutor)
     REFERENCES cemOctanas.Usuario (idUsuario),
@@ -67,32 +65,35 @@ CREATE TABLE IF NOT EXISTS cemOctanas.Materia (
 
 
 -- -----------------------------------------------------
--- Tabela Usuario_Materia
+-- Tabela cemOctanas.Usuario_Materia
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Usuario_Materia (
   fkUsuario INT NOT NULL,
   fkMateria INT NOT NULL,
   sequencial INT NOT NULL,
-  segundosLidos INT,
-  acesso DATETIME NOT NULL,
+  segundosLidos INT NOT NULL,
+  acesso TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (fkUsuario, fkMateria, sequencial),
-  CONSTRAINT fk_Usuario_has_Materia_Usuario
+  INDEX idx_materia_usuario (fkMateria, fkUsuario),
+  INDEX idx_usuario_materia (fkUsuario, fkMateria),
+  CONSTRAINT fk_Usuario_Materia
     FOREIGN KEY (fkUsuario)
     REFERENCES cemOctanas.Usuario (idUsuario),
-  CONSTRAINT fk_Usuario_has_Materia_Materia1
+  CONSTRAINT fk_Materia_Usuario
     FOREIGN KEY (fkMateria)
     REFERENCES cemOctanas.Materia (idMateria)
 );
 
 
 -- -----------------------------------------------------
--- Tabela Newsletter
+-- Tabela cemOctanas.Newsletter
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Newsletter (
-  idNewsletter INT NOT NULL,
-  email VARCHAR(45) NULL,
+  idNewsletter INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(200) NOT NULL,
   fkUsuario INT NULL,
   PRIMARY KEY (idNewsletter),
+  INDEX idx_email_newsletter (email),
   CONSTRAINT fk_Newsletter_Usuario
     FOREIGN KEY (fkUsuario)
     REFERENCES cemOctanas.Usuario (idUsuario)
@@ -100,48 +101,54 @@ CREATE TABLE IF NOT EXISTS cemOctanas.Newsletter (
 
 
 -- -----------------------------------------------------
--- Tabela Categoria
+-- Tabela cemOctanas.Categoria
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Categoria (
-  idCategoria INT NOT NULL,
-  Categoria VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idCategoria)
+  idCategoria INT NOT NULL AUTO_INCREMENT,
+  Categoria VARCHAR(50) NOT NULL,
+  PRIMARY KEY (idCategoria),
+  INDEX idx_categoria (Categoria)
 );
 
 
 -- -----------------------------------------------------
--- Tabela Categoria_Materia
+-- Tabela cemOctanas.Categoria_Materia
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS cemOctanas.Categoria_Materia (
   fkMateria INT NOT NULL,
   fkCategoria INT NOT NULL,
   nivel TINYINT NOT NULL,
   PRIMARY KEY (fkMateria, fkCategoria, nivel),
-  CONSTRAINT fk_Categoria_Materia1
+  INDEX idx_materia (fkMateria, fkCategoria),
+  CONSTRAINT fk_Categoria_Materia
     FOREIGN KEY (fkCategoria)
     REFERENCES cemOctanas.Categoria (idCategoria),
-  CONSTRAINT fk_Categoria_Materia2
+  CONSTRAINT fk_Materia_Categoria
     FOREIGN KEY (fkMateria)
     REFERENCES cemOctanas.Materia (idMateria)
 );
 
 
 -- -----------------------------------------------------
--- Tabela Contato
+-- Tabela cemOctanas.InfoUsuario
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS cemOctanas.Contato (
-  idContato INT NOT NULL,
-  ddi CHAR(2) NOT NULL,
-  ddd CHAR(2) NOT NULL,
-  telefone CHAR(9) NOT NULL,
-  email VARCHAR(45) NOT NULL,
-  fkUsuario INT NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS cemOctanas.InfoUsuario (
+  idContato INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(200) NOT NULL,
+  dtInsc DATE NOT NULL,
+  foto VARCHAR(70) NOT NULL,
+  ativo TINYINT NULL DEFAULT 1,
+  banido TINYINT NULL,
+  verificado TINYINT NOT NULL DEFAULT 0,
+  fkUsuario INT NOT NULL,
   PRIMARY KEY (idContato),
+  UNIQUE INDEX unique_email (email, ativo),
+  UNIQUE INDEX unique_bloqueado (email, banido),
+  INDEX idx_email (email),
   CONSTRAINT fk_Contato_Usuario
     FOREIGN KEY (fkUsuario)
     REFERENCES cemOctanas.Usuario (idUsuario)
 );
-
 
 -- -----------------------------------------------------
 -- Procedimento
