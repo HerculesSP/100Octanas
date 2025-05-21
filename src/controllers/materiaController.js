f711b5c3f10bf802a91c78cdc0c4e0550a3dc319
Hércules <herculespereira99@gmail.com>
2025-05-20 23:47:49 -0300

    MVC da tela de matérias com todos os valores na model ( Multer não está retornando o nome no vetor)

--- src/controllers/materiaController.js
+++ src/controllers/materiaController.js
@@ -6,10 +6,26 @@ exports.mostrarMateria = (req, res) => {
 };
 
 exports.criarMateria = (req, res) => {
-  const sessionId = req.sessionID;  
-  const { capa, titulo, legenda, link, local, categoria1, categoria2, categoria3, tipo, corpo } = req.body;
-  console.log(capa, titulo, legenda, link, local, categoria1, categoria2, categoria3, tipo, corpo, sessionId)
-
+  const { capa, titulo, legenda, link, local, categoria1, categoria2, categoria3, tipo, corpo, id } = req.body;
+  console.log(capa, titulo, legenda, link, local, categoria1, categoria2, categoria3, tipo, corpo, id)
+  if (tipo && !Array.isArray(tipo)) tipo = [tipo];
+  if (corpo && !Array.isArray(corpo)) corpo = [corpo];
+  let text=''
+  for (let i=0; i<corpo.length; i++){
+    if (tipo[i]=='Subtítulo'){
+      text+=`<h3> ${corpo[i]} </h3>`
+    } else if (tipo[i]=='Paragráfo'){
+      text+=`<p> ${corpo[i]} </p>`
+    } else if (tipo[i]=='Vídeo'){
+      text += `<iframe src="${corpo[i]}" frameborder="0" allowfullscreen></iframe>`;
+    } else if (tipo[i]=='Imagem' && tipo[i+1]=='Legenda'){
+      text += `<img src="res/img/${corpo[i]}" alt="${corpo[i+1]}">`;
+      i++
+    } else if (tipo[i]=='Imagem'){
+      text += `<img src="res/img/${corpo[i]}">`;
+    }
+  }
+  console.log(text)
   //ejs.renderFile('views/template.ejs', { nome, email }, (err, html) => {
     //fs.writeFile('public/template.html', html, (err) => {
       //res.redirect('/template.html');
